f11fafb9b916a4e8eefbce38b4be0c0dd8232eee
Joonyoung Sung Desktop <sungso376@gmail.com>
2025-11-24 02:06:07 +0900

    Activation func

---
+++ equipment_header/sungso376_AF.hpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+//sigmod
+
+// // 또는 C++11 이후의 트레일링 반환(Trailing return) 구문을 사용하여
+// // 반환형을 더 유연하게 지정할 수도 있습니다.
+// template <typename T1, typename T2>
+// auto add_flexible(T1 a, T2 b) -> decltype(a + b) {
+//     return a + b;
+// }
