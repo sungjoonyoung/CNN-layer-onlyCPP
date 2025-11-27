8efa6996e606965b6efffae42de6e60c4f3cc36d
Joonyoung Sung Desktop <sungso376@gmail.com>
2025-11-28 06:12:53 +0900

    header

--- 000.equipment_header/sungso376_AF.hpp
+++ 000.equipment_header/sungso376_AF.hpp
@@ -19,6 +19,17 @@ template <typename T1>
 auto ReLU(T1 x)->decltype(x){
     return max(x,(T1)0);
 }
+
+
+
+
+/*
+derivative
+*/
+
+
+
+
 // // 또는 C++11 이후의 트레일링 반환(Trailing return) 구문을 사용하여
 // // 반환형을 더 유연하게 지정할 수도 있습니다.
 // template <typename T1, typename T2>
